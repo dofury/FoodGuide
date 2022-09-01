b957090acca90da9166176ea6672cc6e684ca63c
dofury <cha_dh1004@naver.com>
2022-09-01 19:00:26 +0900

    테이블 음식아이콘 터치액션구현

--- app/src/main/java/com/dofury/foodguide/Table.java
+++ app/src/main/java/com/dofury/foodguide/Table.java
@@ -1,13 +1,17 @@
 package com.dofury.foodguide;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,6 +23,9 @@ public class Table extends Fragment {
     Food selectedFood;
     String preFrag = "dd";
     Bundle bundle;
+    private ViewGroup mainLayout;
+    private int xDelta;
+    private int yDelta;
     Food testA;
 
 
@@ -57,7 +64,47 @@ public class Table extends Fragment {
                 preFrag = "";
                 break;
         }
+        mainLayout = (LinearLayout) view.findViewById(R.id.main);
+        imageView = view.findViewById(R.id.appetizer_image);
+        imageView.setOnTouchListener(onTouchListener());
         return view;
+    }
+    private View.OnTouchListener onTouchListener() {
+        return (view, event) -> {
+
+                final int x = (int) event.getRawX();
+                final int y = (int) event.getRawY();
+
+                switch (event.getAction() & MotionEvent.ACTION_MASK) {
+
+                    case MotionEvent.ACTION_DOWN:
+                        LinearLayout.LayoutParams lParams = (LinearLayout.LayoutParams)
+                                view.getLayoutParams();
+
+                        xDelta = x - lParams.leftMargin;
+                        yDelta = y - lParams.topMargin;
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        Toast.makeText(getActivity(), " I'm here", Toast.LENGTH_SHORT)
+                                .show();
+                        break;
+
+                    case MotionEvent.ACTION_MOVE:
+                        LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) view.getLayoutParams();
+                        layoutParams.leftMargin = x - xDelta;
+                        layoutParams.topMargin = y - yDelta;
+                        layoutParams.rightMargin = 0;
+                        layoutParams.bottomMargin = 0;
+                        view.setLayoutParams(layoutParams);
+                        break;
+                }
+
+                mainLayout.invalidate();
+                return true;
+
+
+        };
     }
     private void selectFood(){
         imageView = view.findViewById(R.id.appetizer_image);
