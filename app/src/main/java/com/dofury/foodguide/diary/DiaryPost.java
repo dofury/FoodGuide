510fd7114ac3455c01c19f0508b582434e74e9c0
도훈 차 <cha_dh1004@naver.com>
2022-09-10 03:04:02 +0900

    다이어리 이미지 업로드 기능 추가

--- app/src/main/java/com/dofury/foodguide/diary/DiaryPost.java
+++ app/src/main/java/com/dofury/foodguide/diary/DiaryPost.java
@@ -1,25 +1,38 @@
 package com.dofury.foodguide.diary;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.dofury.foodguide.Activity;
 import com.dofury.foodguide.Food;
 import com.dofury.foodguide.R;
 import com.dofury.foodguide.community.CommunityAddActivity;
 import com.dofury.foodguide.login.UserAccount;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -28,6 +41,9 @@ public class DiaryPost extends AppCompatActivity {
     private final UserAccount userAccount = UserAccount.getInstance();
     private final DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("FoodGuide");
     Food food;
+    Button diaryImageBtn;
+    ImageView diaryImageView;
+    Uri uri;
     public DiaryPost() {
 
     }
@@ -40,10 +56,36 @@ public class DiaryPost extends AppCompatActivity {
         findViewById(R.id.btn_diary_save).setOnClickListener(onClickListener);
         Intent intent = getIntent();
         food = intent.getParcelableExtra("DiaryPost");
-
-
+        diaryImageBtn= findViewById(R.id.btn_diary_image_select);
+        diaryImageView= findViewById(R.id.iv_diary_image);
+        diaryImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                upLoadImage();
+            }
+        });
 
     }
+    private void upLoadImage() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+
+        launcher.launch(intent);
+    }
+
+    ActivityResultLauncher<Intent> launcher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                        uri = result.getData().getData();
+                        Glide.with(DiaryPost.this).load(uri).into(diaryImageView);
+
+                    }
+                }
+            });
+
     View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
@@ -71,19 +113,48 @@ public class DiaryPost extends AppCompatActivity {
         }
 
     }
-    private void uploader(PostInfo postInfo,String key){
-        databaseReference.child("UserAccount").child(userAccount.getIdToken()).child("food").child(food.getId()).child(key).setValue(postInfo).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if(task.isSuccessful()) {
-                    Toast.makeText(DiaryPost.this, "게시물이 등록되었습니다.", Toast.LENGTH_SHORT).show();
-                    onBackPressed();
-                }
-                else {
-                    Toast.makeText(DiaryPost.this, "오류가 발생했습니다.", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+    private void uploader(PostInfo postInfo,String key) {
+        if (uri != null) {
+            StorageReference storageReference = FirebaseStorage.getInstance().getReference("Diary");
+            storageReference.child(key).child("image.png").putFile(uri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        storageReference.child(key).child("image.png").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                postInfo.setImage(uri.toString());
+                                databaseReference.child("UserAccount").child(userAccount.getIdToken()).child("food").child(food.getId()).child(key).setValue(postInfo).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()) {
+                                            Toast.makeText(DiaryPost.this, "게시물이 등록되었습니다.", Toast.LENGTH_SHORT).show();
+                                            onBackPressed();
+                                        } else {
+                                            Toast.makeText(DiaryPost.this, "오류가 발생했습니다.", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
 
+                            }
+                        });
+                    }
+                }
+            });
+        } else {
+            databaseReference.child("UserAccount").child(userAccount.getIdToken()).child("food").child(food.getId()).child(key).setValue(postInfo).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(DiaryPost.this, "게시물이 등록되었습니다.", Toast.LENGTH_SHORT).show();
+                        onBackPressed();
+                    } else {
+                        Toast.makeText(DiaryPost.this, "오류가 발생했습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+        }
     }
+
 }
--- app/src/main/java/com/dofury/foodguide/diary/PostInfo.java
+++ app/src/main/java/com/dofury/foodguide/diary/PostInfo.java
@@ -3,15 +3,24 @@ package com.dofury.foodguide.diary;
 public class PostInfo {
     String title;
     String contents;
+    String image;
 
     public PostInfo(String title,String contents)
     {
         this.title = title;
         this.contents = contents;
+        this.image = image;
     }
 
     public String getTitle() { return this.title;}
     public String getContents() { return this.contents;}
     public void setTitle(String title) { this.title = title;}
     public void setContents(String contents) { this.contents = contents;}
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
